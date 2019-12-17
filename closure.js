fac10cb559a248cd481cd47992ae620016020555
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-12-17 21:48:54 +0530

    Added closure example with param

--- closure.js
+++ closure.js
@@ -1,7 +1,14 @@
 const numbers = [1, 2, 3, 4];
 
-const reducer = () => {
-  return (acc, number) => acc + number;
+// const reducer = () => {
+//   return (acc, number) => acc + number;
+// };
+// const sum = numbers.reduce(reducer());
+// console.log(sum);
+
+const reducer = num => {
+  return (acc, number) => acc.concat(number * num);
 };
 
-console.log(numbers.reduce(reducer()));
+const multipliedValues = numbers.reduce(reducer(2), []);
+console.log(multipliedValues);
